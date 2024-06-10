a552c31e0ad504b521b4c9a45c1bed9f63a81da1
kimjunho0322 <jzzazza@naver.com>
2024-06-10 20:21:25 +0900

    tnwjd
    수정

--- src/main/java/SchoolProject/Backend/controller/CommentController.java
+++ src/main/java/SchoolProject/Backend/controller/CommentController.java
@@ -1,27 +1,18 @@
 package SchoolProject.Backend.controller;
 
 import SchoolProject.Backend.dto.CommentDTO;
-import SchoolProject.Backend.entity.Comment;
 import SchoolProject.Backend.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/comments")
 public class CommentController {
     private final CommentService commentService;
 
-    @PostMapping
+    @PostMapping("/comments")
     public String submitComment(@RequestBody CommentDTO commentDTO) {
         commentService.saveComment(commentDTO);
         return "good";
-    }
-
-    @GetMapping
-    public List<Comment> getAllComments() {
-        return commentService.findAllComments();
     }
 }
